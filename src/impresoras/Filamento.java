b9b908960830beff180f09786daadbe071e5e6eb
vlzJuan <valencellojuan@gmail.com>
2025-05-31 18:31:44 -0300

    [Filamento]: - Creación del método 'filamentoFromId', que retorna el filamento acorde a su ID si es valido, o 'null' si no lo es. - Eliminación del método 'isInIdentifiers', deprecado con el uso del nuevo método 'filamentoFromId'. - Añadido menor a la documentación de los métodos.

--- src/impresoras/Filamento.java
+++ src/impresoras/Filamento.java
@@ -11,25 +11,34 @@ public enum Filamento {
     private final char identificador;
     private final String color;
 
-
+    // Constructor para los filamento constantes.
     Filamento(char identifier, String color){
         this.identificador = identifier;
         this.color = color;
     }
 
-    public static boolean isInIdentifiers(char test){
-
-        return identifiers.indexOf(test)>=0;
+    /**
+     * Metodo para retornar un filamento, en función de su identificador.
+     *
+     * @param id    , un caracter que identifica al color buscado.
+     * @return      el Filamento apropiado al identificador si es valido, o 'null' otherwise.
+     */
+    public static Filamento filamentoFromId(char id) {
+        Filamento ret = null;
+        for (Filamento f : values()) {
+            if (f.identificador == id) {
+                ret = f;
+            }
+        }
+        return ret;
     }
 
+    // Getters de identificador y color.
     public char getId(){
         return this.identificador;
     }
-
     String getColor(){
         return this.color;
     }
-
-
+    
 }
-
