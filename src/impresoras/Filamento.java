76065326fe75d36e184d1e7df5fd93fadb3217d2
vlzJuan <valencellojuan@gmail.com>
2025-05-31 17:07:18 -0300

    Comportamiento de [Filamento] modificado. Ahora implementa una función pública para verificar si un identificador está entre los colores soportados.
    Se añadió un atributo privado 'identificador'.

--- src/impresoras/Filamento.java
+++ src/impresoras/Filamento.java
@@ -1,16 +1,29 @@
 package impresoras;
 
 public enum Filamento {
-    BLANCO("White"),
-    NEGRO("Black"),
-    AZUL("Blue"),
-    ROJO("Red"),
-    VERDE("Green");
+    BLANCO('B',"Blanco"),
+    NEGRO('N',"Negro"),
+    AZUL('A',"Azul"),
+    ROJO('R',"Rojo"),
+    VERDE('V',"Verde");
 
+    private static final String identifiers = "BNARV";
+    private final char identificador;
     private final String color;
 
-    Filamento(String color){
+
+    Filamento(char identifier, String color){
+        this.identificador = identifier;
         this.color = color;
+    }
+
+    public static boolean isInIdentifiers(char test){
+
+        return identifiers.indexOf(test)>=0;
+    }
+
+    public char getId(){
+        return this.identificador;
     }
 
     String getColor(){
