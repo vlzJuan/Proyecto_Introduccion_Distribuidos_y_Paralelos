e66a1ff864393defdc6bcfb42466ee77429e72fb
vlzJuan <valencellojuan@gmail.com>
2025-06-07 16:44:32 -0300

    Se creo la interfaz (I)[Listener] y una clase Logger que la implementa.

---
+++ src/logger/Listener.java
@@ -0,0 +1,10 @@
+package logger;
+
+/**
+ * Interface para objeto que loggee mensajes
+ */
+public interface Listener {
+
+    public void log(String msj);
+
+}
---
+++ src/logger/Logger.java
@@ -0,0 +1,16 @@
+package logger;
+
+
+/**
+ * Este logger funciona para imprimir las cosas por consola.
+ */
+public class Logger implements Listener{
+
+
+
+    public void log(String msj){
+        System.out.println(msj);
+    }
+
+
+}
